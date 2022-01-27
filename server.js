c69766f9bb87ffa5b3a34fb0e53f8ae2aa462cc3
sikamloïque <loicsikam272@gmail.com>
2022-01-27 16:21:54 +0100

    first heroku

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use('/api', planningRoute);
 // });
 
 app.get('*', (req, res) =>{
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+  res.sendFile(path.join(__dirname + '/public/src/index.html'));
 })
 
 const PORT = process.env.PORT || 3000;
