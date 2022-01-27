642c8fc459622081a4d0a4d53c8a068b821f7da8
sikamloïque <loicsikam272@gmail.com>
2022-01-27 16:24:14 +0100

    first heroku

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use('/api', planningRoute);
 //     res.json({ message: "bienvenue sur le serveur" });
 // });
 
-app.get('*', (req, res) =>{
+app.get('/', (req, res) =>{
   res.sendFile(path.join(__dirname + '/public/src/index.html'));
 })
 
